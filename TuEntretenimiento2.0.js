6d698c13af76f882a0833e5e3e9ef49dab233156
LuigiP99 <79221381+LuigiP99@users.noreply.github.com>
2021-02-26 19:14:42 -0400

    Add files via upload

---
+++ TuEntretenimiento2.0.js
@@ -0,0 +1,12 @@
+function cambiarModo() {
+    var cuerpoweb = document.body;
+    cuerpoweb.classList.toggle("oscuro");
+}
+
+window.onload=function(){
+    alert('Bienvenido a TuEntretenimiento2.0');
+}
+window.onunload=function(){
+    alert('Gracias por visitar TuEntretenimiento2.0');
+}
+
